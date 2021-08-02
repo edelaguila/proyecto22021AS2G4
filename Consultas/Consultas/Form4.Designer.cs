260ec4ed88761b648add1d4e3948971e2ffdcd9e
Frank <francisco2015franco@gmail.com>
2021-08-01 22:06:43 -0600

    Segunda parte del prototipo

---
+++ Consultas/Consultas/Form4.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Consultas
+{
+    partial class Telefono
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Telefono));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(190, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Llenar los siguientes campos ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(267, 248);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID telefono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefono";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(467, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 80);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Telefono
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 381);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Telefono";
+            this.Text = "Telefono";
+            this.Load += new System.EventHandler(this.Telefono_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Consultas/Consultas/Form4.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Consultas
+{
+    public partial class Telefono : Form
+    {
+        public Telefono()
+        {
+            InitializeComponent();
+        }
+
+        private void Telefono_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form1();
+            frm.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+                   
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+                    
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
