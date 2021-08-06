81b1fc043aa5570e7244352de5f4128285344a64
Frank <francisco2015franco@gmail.com>
2021-08-05 18:13:10 -0600

    Prototipo unificado 1.0

---
+++ prototipo/ventana_maestros/ventana_maestros/FormCatedratico.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ventana_maestros
+{
+    public partial class FormCatedratico : Form
+    {
+        public FormCatedratico()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form frm = new frmMaestros();
+            frm.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox T = (TextBox)sender;
+            try
+            {
+                //Not Allowing Numbers
+                char[] UnallowedCharacters = { '0', '1',
+                                           '2', '3',
+                                           '4', '5',
+                                           '6', '7',
+                                           '8', '9'};
+
+                if (textContainsUnallowedCharacter(T.Text, UnallowedCharacters))
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+                    //Align Cursor to same index
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private bool textContainsUnallowedCharacter(string T, char[] UnallowedCharacters)
+        {
+            for (int i = 0; i < UnallowedCharacters.Length; i++)
+                if (T.Contains(UnallowedCharacters[i]))
+                    return true;
+
+            return false;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+}
---
+++ prototipo/ventana_maestros/ventana_maestros/FormDireccion.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ventana_maestros
+{
+    partial class FormDireccion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 298);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código Postal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "lngrese Dirección";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Departamento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Municipio";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 255);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(192, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(217, 298);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(192, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(217, 147);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(192, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(199, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "lngreso de dirrección";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(214, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "lngrese los siguientes campos";
+            // 
+            // FormDireccion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 408);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDireccion";
+            this.Text = "FormDireccion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
