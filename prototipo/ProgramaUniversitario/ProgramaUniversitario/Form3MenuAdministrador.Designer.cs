06d1f32f7865d88554b30f2d8cc8130ed65ab988
BuildTools <unconfigured@null.spigotmc.org>
2021-08-07 18:55:14 -0600

    actualizacion de ingreso catedratico y ventanas de reportes

--- prototipo/ProgramaUniversitario/ProgramaUniversitario/Form3MenuAdministrador.Designer.cs
+++ prototipo/ProgramaUniversitario/ProgramaUniversitario/Form3MenuAdministrador.Designer.cs
@@ -31,14 +31,14 @@ namespace ProgramaUniversitario
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegistroCatedratico = new System.Windows.Forms.Button();
             this.btnAyuda = new System.Windows.Forms.Button();
             this.btnCerrar = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnGestionUsuario = new System.Windows.Forms.Button();
             this.btnReporteSeccion = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btnReporteCarrera = new System.Windows.Forms.Button();
+            this.btnReporteCurso = new System.Windows.Forms.Button();
+            this.btnReporteCatedraticos = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -47,10 +47,9 @@ namespace ProgramaUniversitario
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ProgramaUniversitario.Properties.Resources.Login;
-            this.pictureBox1.Location = new System.Drawing.Point(81, 48);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(61, 39);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(185, 164);
+            this.pictureBox1.Size = new System.Drawing.Size(139, 133);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -59,29 +58,27 @@ namespace ProgramaUniversitario
             // 
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Kollektif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(77, 247);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(58, 201);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 29);
+            this.label1.Size = new System.Drawing.Size(142, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "Administrador";
             // 
-            // button1
+            // btnRegistroCatedratico
             // 
-            this.button1.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.DarkRed;
-            this.button1.Location = new System.Drawing.Point(380, 48);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 27);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Registro Catedratico";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnRegistroCatedratico.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
+            this.btnRegistroCatedratico.FlatAppearance.BorderSize = 0;
+            this.btnRegistroCatedratico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistroCatedratico.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnRegistroCatedratico.Location = new System.Drawing.Point(285, 39);
+            this.btnRegistroCatedratico.Name = "btnRegistroCatedratico";
+            this.btnRegistroCatedratico.Size = new System.Drawing.Size(136, 22);
+            this.btnRegistroCatedratico.TabIndex = 3;
+            this.btnRegistroCatedratico.Text = "Registro Catedratico";
+            this.btnRegistroCatedratico.UseVisualStyleBackColor = true;
+            this.btnRegistroCatedratico.Click += new System.EventHandler(this.btnRegistroCatedratico_Click);
             // 
             // btnAyuda
             // 
@@ -89,10 +86,9 @@ namespace ProgramaUniversitario
             this.btnAyuda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnAyuda.FlatAppearance.BorderSize = 0;
             this.btnAyuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAyuda.Location = new System.Drawing.Point(977, 482);
-            this.btnAyuda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAyuda.Location = new System.Drawing.Point(733, 392);
             this.btnAyuda.Name = "btnAyuda";
-            this.btnAyuda.Size = new System.Drawing.Size(73, 57);
+            this.btnAyuda.Size = new System.Drawing.Size(55, 46);
             this.btnAyuda.TabIndex = 5;
             this.btnAyuda.UseVisualStyleBackColor = true;
             // 
@@ -102,27 +98,25 @@ namespace ProgramaUniversitario
             this.btnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnCerrar.FlatAppearance.BorderSize = 0;
             this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCerrar.Location = new System.Drawing.Point(977, 15);
-            this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrar.Location = new System.Drawing.Point(733, 12);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(73, 57);
+            this.btnCerrar.Size = new System.Drawing.Size(55, 46);
             this.btnCerrar.TabIndex = 6;
             this.btnCerrar.UseVisualStyleBackColor = true;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
-            // button3
+            // btnGestionUsuario
             // 
-            this.button3.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.Color.DarkRed;
-            this.button3.Location = new System.Drawing.Point(380, 82);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(181, 27);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "Gestion de Usuario";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnGestionUsuario.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
+            this.btnGestionUsuario.FlatAppearance.BorderSize = 0;
+            this.btnGestionUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGestionUsuario.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnGestionUsuario.Location = new System.Drawing.Point(285, 67);
+            this.btnGestionUsuario.Name = "btnGestionUsuario";
+            this.btnGestionUsuario.Size = new System.Drawing.Size(136, 22);
+            this.btnGestionUsuario.TabIndex = 7;
+            this.btnGestionUsuario.Text = "Gestion de Usuario";
+            this.btnGestionUsuario.UseVisualStyleBackColor = true;
             // 
             // btnReporteSeccion
             // 
@@ -130,84 +124,82 @@ namespace ProgramaUniversitario
             this.btnReporteSeccion.FlatAppearance.BorderSize = 0;
             this.btnReporteSeccion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReporteSeccion.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnReporteSeccion.Location = new System.Drawing.Point(380, 117);
-            this.btnReporteSeccion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReporteSeccion.Location = new System.Drawing.Point(285, 95);
             this.btnReporteSeccion.Name = "btnReporteSeccion";
-            this.btnReporteSeccion.Size = new System.Drawing.Size(261, 27);
+            this.btnReporteSeccion.Size = new System.Drawing.Size(196, 22);
             this.btnReporteSeccion.TabIndex = 8;
             this.btnReporteSeccion.Text = "Reporte de alumnos por seccion";
             this.btnReporteSeccion.UseVisualStyleBackColor = true;
+            this.btnReporteSeccion.Click += new System.EventHandler(this.btnReporteSeccion_Click);
             // 
-            // button4
+            // btnReporteCarrera
             // 
-            this.button4.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.ForeColor = System.Drawing.Color.DarkRed;
-            this.button4.Location = new System.Drawing.Point(380, 151);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(257, 27);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Reporte de alumnos por carrera";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnReporteCarrera.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
+            this.btnReporteCarrera.FlatAppearance.BorderSize = 0;
+            this.btnReporteCarrera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporteCarrera.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReporteCarrera.Location = new System.Drawing.Point(285, 123);
+            this.btnReporteCarrera.Name = "btnReporteCarrera";
+            this.btnReporteCarrera.Size = new System.Drawing.Size(193, 22);
+            this.btnReporteCarrera.TabIndex = 9;
+            this.btnReporteCarrera.Text = "Reporte de alumnos por carrera";
+            this.btnReporteCarrera.UseVisualStyleBackColor = true;
+            this.btnReporteCarrera.Click += new System.EventHandler(this.btnReporteCarrera_Click);
             // 
-            // button5
+            // btnReporteCurso
             // 
-            this.button5.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.ForeColor = System.Drawing.Color.DarkRed;
-            this.button5.Location = new System.Drawing.Point(380, 186);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(251, 27);
-            this.button5.TabIndex = 10;
-            this.button5.Text = "Reporte de alumnos por curso";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnReporteCurso.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
+            this.btnReporteCurso.FlatAppearance.BorderSize = 0;
+            this.btnReporteCurso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporteCurso.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReporteCurso.Location = new System.Drawing.Point(285, 151);
+            this.btnReporteCurso.Name = "btnReporteCurso";
+            this.btnReporteCurso.Size = new System.Drawing.Size(188, 22);
+            this.btnReporteCurso.TabIndex = 10;
+            this.btnReporteCurso.Text = "Reporte de alumnos por curso";
+            this.btnReporteCurso.UseVisualStyleBackColor = true;
+            this.btnReporteCurso.Click += new System.EventHandler(this.btnReporteCurso_Click);
             // 
-            // button6
+            // btnReporteCatedraticos
             // 
-            this.button6.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
-            this.button6.FlatAppearance.BorderSize = 0;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.ForeColor = System.Drawing.Color.DarkRed;
-            this.button6.Location = new System.Drawing.Point(380, 220);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(212, 27);
-            this.button6.TabIndex = 11;
-            this.button6.Text = "Reporte de catedraticos";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnReporteCatedraticos.BackgroundImage = global::ProgramaUniversitario.Properties.Resources.fondo;
+            this.btnReporteCatedraticos.FlatAppearance.BorderSize = 0;
+            this.btnReporteCatedraticos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReporteCatedraticos.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReporteCatedraticos.Location = new System.Drawing.Point(285, 179);
+            this.btnReporteCatedraticos.Name = "btnReporteCatedraticos";
+            this.btnReporteCatedraticos.Size = new System.Drawing.Size(159, 22);
+            this.btnReporteCatedraticos.TabIndex = 11;
+            this.btnReporteCatedraticos.Text = "Reporte de catedraticos";
+            this.btnReporteCatedraticos.UseVisualStyleBackColor = true;
+            this.btnReporteCatedraticos.Click += new System.EventHandler(this.btnReporteCatedraticos_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ProgramaUniversitario.Properties.Resources.logo;
-            this.pictureBox2.Location = new System.Drawing.Point(16, 482);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(12, 392);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(73, 57);
+            this.pictureBox2.Size = new System.Drawing.Size(55, 46);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
             // 
             // Form3MenuAdministrador
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnReporteCatedraticos);
+            this.Controls.Add(this.btnReporteCurso);
+            this.Controls.Add(this.btnReporteCarrera);
             this.Controls.Add(this.btnReporteSeccion);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnGestionUsuario);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.btnAyuda);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRegistroCatedratico);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3MenuAdministrador";
             this.Text = "🎓 | Menu Administrador";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -221,14 +213,14 @@ namespace ProgramaUniversitario
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegistroCatedratico;
         private System.Windows.Forms.Button btnAyuda;
         private System.Windows.Forms.Button btnCerrar;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnGestionUsuario;
         private System.Windows.Forms.Button btnReporteSeccion;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnReporteCarrera;
+        private System.Windows.Forms.Button btnReporteCurso;
+        private System.Windows.Forms.Button btnReporteCatedraticos;
         private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
---
+++ prototipo/ProgramaUniversitario/ProgramaUniversitario/Form4IngresoCatedratico.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaUniversitario
+{
+    public partial class Form4IngresoCatedratico : Form
+    {
+        Conexion cn = new Conexion();
+        public Form4IngresoCatedratico()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(Application.StartupPath + @"\img\fondo.jpg");
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btnregresar_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form3MenuAdministrador();
+            frm.Show();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Form frm = new Form1menuprincipal();
+            frm.Show();
+        }
+
+        private void btnguardar_Click(object sender, EventArgs e)
+        {
+            insertarCatedratico();
+            txtIdcatedratico.Text = "";
+            txtnombres.Text = "";
+            txtdpi.Text = "";
+            txtcorreo.Text = "";
+            txtdepartamento.Text = "";
+            txtcodigopostal.Text = "";
+            txtapellidos.Text = "";
+            txttelefono.Text = "";
+            txtdireccion.Text = "";
+            txtmunicipio.Text = "";
+        }
+        // metodo para insertar
+        void insertarCatedratico()
+        {
+            try
+            {
+                string cadena = "INSERT INTO catedratico (idCat, nombres, apellidos, dpi)" + 
+                "VALUES ('" + txtIdcatedratico.Text + "','" + txtnombres.Text + "','" + txtapellidos.Text + "','" + txtdpi + "');";
+                OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+                
+                string cadena2 = "INSERT INTO direcciones (idDir, Diereccion, Departamento, municipio, codPostal)" +
+                "VALUES ('1','" + txtdireccion + "','" + txtdepartamento + "','" + txtdepartamento + "','" + txtcodigopostal + "');";
+                OdbcCommand consulta2 = new OdbcCommand(cadena2, cn.conexion());
+
+                string cadena3 = "INSERT INTO correos (idCorreo, Correo)" +
+                "VALUES ('1','" + txtcorreo + "');";
+                OdbcCommand consulta3 = new OdbcCommand(cadena3, cn.conexion());
+
+                string cadena4 = "INSERT INTO telefonos (idTelefono, Telefono)" +
+               "VALUES ('1','" + txttelefono.Text + "');";
+                OdbcCommand consulta4 = new OdbcCommand(cadena4, cn.conexion());
+                consulta.ExecuteNonQuery();
+            }
+            catch(Exception Error)
+            {
+                Console.WriteLine("Error al Guardar" + Error);
+            }
+
+        }
+
+        private void txtIdcatedratico_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void txtnombres_TextChanged(object sender, EventArgs e)
+        {
+            TextBox T = (TextBox)sender;
+            try
+            {
+
+                char[] UnallowedCharacters = { '0', '1',
+                                           '2', '3',
+                                           '4', '5',
+                                           '6', '7',
+                                           '8', '9'};
+
+                if (textContainsUnallowedCharacter(T.Text, UnallowedCharacters))
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+            }
+            catch (Exception) { }
+        }
+        private bool textContainsUnallowedCharacter(string T, char[] UnallowedCharacters)
+        {
+            for (int i = 0; i < UnallowedCharacters.Length; i++)
+                if (T.Contains(UnallowedCharacters[i]))
+                    return true;
+
+            return false;
+        }
+
+        private void txtdpi_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void txtdepartamento_TextChanged(object sender, EventArgs e)
+        {
+            TextBox T = (TextBox)sender;
+            try
+            {
+
+                char[] UnallowedCharacters = { '0', '1',
+                                           '2', '3',
+                                           '4', '5',
+                                           '6', '7',
+                                           '8', '9'};
+
+                if (textContainsUnallowedCharacter(T.Text, UnallowedCharacters))
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void txtcodigopostal_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void txtapellidos_TextChanged(object sender, EventArgs e)
+        {
+            TextBox T = (TextBox)sender;
+            try
+            {
+
+                char[] UnallowedCharacters = { '0', '1',
+                                           '2', '3',
+                                           '4', '5',
+                                           '6', '7',
+                                           '8', '9'};
+
+                if (textContainsUnallowedCharacter(T.Text, UnallowedCharacters))
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void txttelefono_TextChanged(object sender, EventArgs e)
+        {
+            Exception X = new Exception();
+
+            TextBox T = (TextBox)sender;
+
+            try
+            {
+                if (T.Text != "-")
+                {
+                    int x = int.Parse(T.Text);
+                }
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void txtmunicipio_TextChanged(object sender, EventArgs e)
+        {
+            TextBox T = (TextBox)sender;
+            try
+            {
+
+                char[] UnallowedCharacters = { '0', '1',
+                                           '2', '3',
+                                           '4', '5',
+                                           '6', '7',
+                                           '8', '9'};
+
+                if (textContainsUnallowedCharacter(T.Text, UnallowedCharacters))
+                {
+                    int CursorIndex = T.SelectionStart - 1;
+                    T.Text = T.Text.Remove(CursorIndex, 1);
+
+
+                    T.SelectionStart = CursorIndex;
+                    T.SelectionLength = 0;
+                }
+            }
+            catch (Exception) { }
+        }
+    }
+}
